8a3bd90787f7741656512f5f76b7c0a85075dbc7
juan cuello gutierrez <jcuellog01@educarex.es>
2025-06-03 12:19:02 +0200

    b

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "595659175"
+    serviceWorkerVersion: "1520207949"
   }
 });
