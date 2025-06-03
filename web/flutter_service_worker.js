b0fa1d963bbe304359146d82f465114514b69212
jcuellog01 <jcuellog01@educarex.es>
2025-06-03 19:17:17 +0200

    fix casa

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -45,17 +45,17 @@ const RESOURCES = {"assets/AssetManifest.bin": "15a02b5405cda2fc92611e33d8809568
 "favicon.png": "38d89048524fbd57bbbfdc275d1b09b5",
 "favicon2.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "5604b4f011340f7018064ca561a1f029",
+"flutter_bootstrap.js": "9ec8a0ee8174f3d64116aa44524ba79d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/logo.png": "4d9f53b24341a1c076b867feeef32751",
-"index.html": "ed261e01a6062daa9f9a226777d9fba9",
-"/": "ed261e01a6062daa9f9a226777d9fba9",
+"index.html": "241899cdb27b7a73b46f20e6efc7a6bc",
+"/": "241899cdb27b7a73b46f20e6efc7a6bc",
 "logo.png": "4d9f53b24341a1c076b867feeef32751",
-"main.dart.js": "ec1bfce04c886edc66b00bbf2dffcb42",
-"manifest.json": "fd091ea5bc8f3ccc10f74bd67179fb6a",
+"main.dart.js": "4faaf4e089fa22db75416dbcf4ff2954",
+"manifest.json": "bf24c84c3bf99672a631c4f84464e793",
 "version.json": "c7886e44279e33d50ab4d86953aa2a62"};
 // The application shell files that are downloaded before a service worker can
 // start.
