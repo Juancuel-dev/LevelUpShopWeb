1941dff24cbdd229c9e30ff61d60f0e7e9f20cdb
juan cuello gutierrez <jcuellog01@educarex.es>
2025-06-03 14:06:32 +0200

    as

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -45,7 +45,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "15a02b5405cda2fc92611e33d8809568
 "favicon.png": "38d89048524fbd57bbbfdc275d1b09b5",
 "favicon2.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "2493f7934ca3c29ea8f1bf00e8190cc2",
+"flutter_bootstrap.js": "ee1e2c6536be24cba47fd8c276290f5b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
